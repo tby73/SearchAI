cebc3891d5eaa730016bbefdb39d0e009b834e2d
Mcy61 <120719507+Mcy61@users.noreply.github.com>
2025-03-07 10:28:45 +0100

    Error search: Connection to the database
    Problem was not found --> Solution: For now i will do the searchbar-Code via Client Data and not server Data. For now, that is the best solution.

--- BackEnd/backend_searchbar.js
+++ BackEnd/backend_searchbar.js
@@ -121,15 +121,15 @@ function read_from_sql(){ //with the help of https://www.w3schools.com/nodejs/no
     
     var connection = mysql.createConnection({
         host: "localhost",
-        user: "",
+        user: "user",
         port: "3306",
-        password: "",
+        password: "password",
         database: "mysql"
     });
 
     console.log(connection);
     console.log("I'm in!");
-    
+
     app.get('/mysql', function(req, res){
         const queryString = "SELECT * FROM your_table";
         connection.query(queryString, function(err, rows, fields){
